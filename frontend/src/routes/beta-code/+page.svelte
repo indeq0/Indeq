2131331743ce67097cc8e4d46d5700132b631504
Amay Patel <amaypatel51@gmail.com>
2025-04-21 21:38:10 -0400

    feat(frontend): more frontend fixes

--- frontend/src/routes/beta-code/+page.svelte
+++ frontend/src/routes/beta-code/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import Button from '$lib/components/ui/button/button.svelte';
   import { InputOTP } from '$lib/components/ui/input-otp/index.js';
+  import { Input } from '$lib/components/ui/input/index.js';
   import { enhance } from '$app/forms';
   import { goto } from '$app/navigation';
   import { toast } from 'svelte-sonner';
@@ -18,6 +19,11 @@
   }
 </script>
 
+<svelte:head>
+  <title>Beta Access | Indeq</title>
+  <meta name="description" content="Beta Access | Indeq" />
+</svelte:head>
+
 <form method="POST" use:enhance>
   <div class="min-h-screen w-full grid lg:grid-cols-2">
     <div class="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
@@ -28,43 +34,52 @@
     </div>
 
     <div class="relative flex items-center justify-center">
-      <div class="mx-auto w-full max-w-[350px] space-y-6 px-4">
-        <div class="flex flex-col space-y-2 text-center">
-          <h1 class="text-2xl font-semibold tracking-tight">Beta Access</h1>
-          <p class="text-muted-foreground text-sm">
-            Enter your beta code and email to check access
+			<div class="mx-auto w-fit space-y-4 px-4">
+				<div class="flex flex-col space-y-2 text-center">
+					<h1 class="text-2xl font-semibold tracking-tight">Beta Access</h1>
+					<p class="text-muted-foreground text-sm">
+						Enter your beta code and email to check access
+					</p>
+				</div>
+
+				<div class="space-y-4 w-full">
+					<InputOTP
+						bind:value={betaCode}
+						length={6}
+						autoFocus
+						mode="alpha"
+					/>
+
+					<div class="relative flex items-center w-full">
+						<div class="flex-grow border-t border-gray-300"></div>
+						<span class="mx-4 text-gray-500 text-sm">and</span>
+						<div class="flex-grow border-t border-gray-300"></div>
+					</div>
+
+					<Input
+						type="email"
+						placeholder="m@example.com"
+						bind:value={email}
+            class="bg-white dark:bg-slate-900"   
+					/>
+
+					<Button type="submit" class="w-full">Check Access</Button>
+
+					{#if form && 'error' in form && form.error}
+						<p class="text-sm text-red-500 text-center">{form.error}</p>
+					{/if}
+				</div>
+
+        <div>
+          <p class="text-muted-foreground px-8 text-center text-sm">
+            Questions?
+            <a
+              href="mailto:support@indeq.app"
+              class="hover:text-primary underline underline-offset-4"
+            >Contact&nbsp;us</a>.
           </p>
         </div>
-
-        <div class="space-y-4">
-          <InputOTP bind:value={betaCode} length={6} autoFocus mode="alpha" />
-          <div class="relative flex items-center">
-            <div class="flex-grow border-t border-gray-300"></div>
-            <span class="mx-4 text-gray-500 text-sm">and</span>
-            <div class="flex-grow border-t border-gray-300"></div>
-          </div>
-          <input
-            type="email"
-            placeholder="m@example.com"
-            bind:value={email}
-            class="w-full rounded-md border px-4 py-2 text-black dark:text-white"
-          />
-          <Button type="submit" class="w-full">Check Access</Button>
-          {#if form && 'error' in form && form.error}
-            <p class="text-sm text-red-500 text-center">{form.error}</p>
-          {/if}
-        </div>
-
-        <p class="text-muted-foreground px-8 text-center text-sm">
-          Questions?
-          <a
-            href="mailto:support@indeq.app"
-            class="hover:text-primary underline underline-offset-4"
-          >
-            Contact us
-          </a>.
-        </p>
-      </div>
-    </div>
+			</div>
+		</div>
   </div>
 </form>
